1baafad17ca2f60278375167b9f8a823575e92c2
Bigit1024 <kgbigit2002@gmail.com>
2024-04-26 17:15:13 +0530

    registration

--- mydb.py
+++ mydb.py
@@ -1,3 +1,7 @@
+#================================================================
+#           EXECUTE ONLY ONCE
+#================================================================
+
 # Install Mysql on your computer 
 # https://dev.mysql.com/downloads/installer/
 # pip install mysql
